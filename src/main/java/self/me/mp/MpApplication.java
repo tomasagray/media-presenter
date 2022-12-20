454d2b2800bac6bdd14d05bce8374495acb3efee
Tomás Gray <tomas.a.gray@gmail.com>
2022-12-19 21:12:42 -0800

    Create project

---
+++ src/main/java/self/me/mp/MpApplication.java
@@ -0,0 +1,12 @@
+package self.me.mp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MpApplication.class, args);
+  }
+}
---
+++ src/test/java/self/me/mp/MpApplicationTests.java
@@ -0,0 +1,11 @@
+package self.me.mp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
