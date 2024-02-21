77f7fb3b43ab571dbf114ccda9f6a0d96de2719d
Tom?s Gray <tomas.a.gray@gmail.com>
2024-02-21 03:07:55 +0000

    Add handler for FileNotFound exception

--- src/main/java/self/me/mp/api/controller/_GlobalExceptionHandler.java
+++ src/main/java/self/me/mp/api/controller/_GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package self.me.mp.api.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,30 +9,46 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class _GlobalExceptionHandler {
 
-	@ExceptionHandler(IllegalArgumentException.class)
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	@ResponseBody
-	public String handleNotFound(@NotNull IllegalArgumentException e) {
-		return e.getMessage();
-	}
+    private static final Logger logger = LogManager.getLogger("GlobalExceptionHandler");
 
-	@ExceptionHandler(NoSuchElementException.class)
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	@ResponseBody
-	public String handleNoElement(@NotNull NoSuchElementException e) {
-		return e.getMessage();
-	}
+    private static String handleError(@NotNull Throwable e) {
+        final String message = e.getMessage();
+        logger.error(message);
+        return message;
+    }
 
-	@ExceptionHandler(MalformedURLException.class)
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	@ResponseBody
-	public String handleBadUrl(@NotNull MalformedURLException e) {
-		return e.getMessage();
-	}
+    @ExceptionHandler(FileNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public String handleFileNotFound(@NotNull FileNotFoundException e) {
+        return handleError(e);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public String handleIllegalArg(@NotNull IllegalArgumentException e) {
+        return handleError(e);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public String handleNoElement(@NotNull NoSuchElementException e) {
+        return handleError(e);
+    }
+
+    @ExceptionHandler(MalformedURLException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public String handleBadUrl(@NotNull MalformedURLException e) {
+        return handleError(e);
+    }
 }
